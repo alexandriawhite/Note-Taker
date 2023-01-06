1ce7235acdd94b42ae5cb4ed482d535d7e3cfb03
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-05 18:39:59 -0600

    Heroku please work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const htmlRoutes = require('./routes/htmlRoutes.js');
 const apiRoutes = require('./routes/apiRoutes.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT||3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
