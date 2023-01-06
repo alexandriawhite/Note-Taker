0d66d9ae6e0d865550428295a23348eb6286a40d
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-05 22:25:25 -0600

    Notes posting to page. Delete not working. Need to correct error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,12 +7,30 @@ const fs = require('fs');
 router.get('/notes', (req, res) => res.json(dbData));
 
 router.post('/notes', (req, res) => {
-let dbData = fs.readFile(req.body)
+dbData.push(req.body)
+    fs.writeFile(dbData, JSON.stringify(dbData), function (err) {
+        if (err) throw err;
+        res.json(db);
+    });
+    
 //bdData fs.writeFile stringify
 // res.json the new note 
 // add ids - wont be able to add or delete until you have ids 
 });
 
 
+// router.delete('notes/:id', function (req, res) {
+//     var id= parseInt(req.params.id);
+//     for (var i = 0; i < dbData.length; i++) {
+// if (db[i].id === id) {
+//     dbData.splice(i,1);
+// }
+// console.log(dbData);
+// fs.writeFile(dbData, JSON.stringify(dbData), function (err) {
+//     if (err) throw err;
+//     res.json(dbData);
+// })
+//     }
+// })
 
 module.exports = router
