dea99b48b0f4c7049aae02ac6a105bb908ee6d10
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-05 18:01:14 -0600

    Pushing to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const path = require('path');
 let dbData = require('../db/db.json');
 const fs = require('fs');
-import { isUuid } from 'uuidv4';
+// import { isUuid } from 'uuidv4';
 
 router.get('/notes', (req, res) => res.json(dbData));
 
 router.post('/notes', (req, res) => {
-req.body
+let dbData = fs.readFile(req.body)
 //bdData fs.writeFile stringify
 // res.json the new note 
 // add ids - wont be able to add or delete until you have ids 
